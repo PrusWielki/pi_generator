8f960fd1c74cf2fd7c119263ed8afb99bb88591c
Patryk <prusak.patryk@accenture.com>
2023-02-03 21:05:06 +0100

    remove unused transition

--- src/components/pi/pi_main_container/pi_main_container.svelte
+++ src/components/pi/pi_main_container/pi_main_container.svelte
@@ -3,7 +3,7 @@
 	let loading: Boolean;
 </script>
 
-<div class="flex w-full flex-row items-center justify-center transition duration-500">
+<div class="flex w-full flex-row items-center justify-center">
 	<Input bind:loading />
 	{#if loading == true}
 		<div class="w-1/2 animate-pulse text-center font-mono text-4xl font-bold">Generating pi...</div>
