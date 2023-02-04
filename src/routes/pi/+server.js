24018b89a7dfe9f888e614ce71cc328fd8b03310
Patryk <prusak.patryk@accenture.com>
2023-02-04 19:13:08 +0100

    :)

--- src/routes/pi/+server.js
+++ src/routes/pi/+server.js
@@ -7,7 +7,7 @@ export async function GET({ url }) {
 	const n = Number(url.searchParams.get('n') ?? '10');
 	const upper = Number(url.searchParams.get('upper') ?? '10');
 	console.log('starting');
-	await exec('.\\program.exe', [
+	await exec('..\\..\\..\\..\\client\\program.exe', [
 		`${n}`,
 		'0',
 		`${n}`,
