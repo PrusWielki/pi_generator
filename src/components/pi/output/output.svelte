34856b5dd72d443ed503fc1810af255667c08d97
Patryk <prusak.patryk@accenture.com>
2023-02-04 18:12:04 +0100

    align both containers

--- src/components/pi/output/output.svelte
+++ src/components/pi/output/output.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <div class="flex flex-col justify-items-center  gap-5">
-	<div class="px-2 py-2">
+	<div class="px-2 py-1">
 		<textarea disabled bind:value={pi} class="h-36 w-full self-start border-2 border-black" />
 	</div>
-	<div class="px-2 py-2">
+	<div class="px-2 py-1">
 		<textarea disabled bind:value={table} class="h-36 w-full self-end border-2 border-black" />
 	</div>
 </div>
