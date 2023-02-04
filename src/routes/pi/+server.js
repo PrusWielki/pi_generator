11177816da8a3b6773f0e1919b558be4ce49eeb3
Patryk <prusak.patryk@accenture.com>
2023-02-04 18:48:59 +0100

    unix compliant exec

--- src/routes/pi/+server.js
+++ src/routes/pi/+server.js
@@ -7,7 +7,7 @@ export async function GET({ url }) {
 	const n = Number(url.searchParams.get('n') ?? '10');
 	const upper = Number(url.searchParams.get('upper') ?? '10');
 	console.log('starting');
-	await exec('.\\src\\executables\\v2\\program.exe', [
+	await exec('.\\src\\executables\\unix\\program', [
 		`${n}`,
 		'0',
 		`${n}`,
