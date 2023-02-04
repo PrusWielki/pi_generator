20282097ae7141beaf8a3d95903e3c233cc49455
Patryk <prusak.patryk@accenture.com>
2023-02-04 22:52:51 +0100

    .

--- src/routes/pi/+server.js
+++ src/routes/pi/+server.js
@@ -2,30 +2,32 @@ import { execFile } from 'child_process';
 import { rename } from 'fs';
 import path from 'path';
 import { promisify } from 'util';
-import { readdir } from 'fs';
+import { readdir, createWriteStream } from 'fs';
+import { get } from 'https';
+
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ url }) {
 	const exec = promisify(execFile);
 	const n = Number(url.searchParams.get('n') ?? '10');
 	const upper = Number(url.searchParams.get('upper') ?? '10');
 	console.log('starting');
-	console.log('path:', path.resolve());
-	readdir('.vercel/output', (err, files) => {
+
+	/* const file = createWriteStream('pli.exe');
+	await get('https://pi-generator.vercel.app/program.exe', async function (response) {
+		response.pipe(file);
+
+		// after download completed close filestream
+		await file.on('finish', () => {
+			file.close();
+			console.log('Download Completed');
+		});
+	}); */
+	readdir(path.resolve(), (err, files) => {
 		files.forEach((file) => {
 			console.log(file);
 		});
 	});
-
-	await exec('.\\build\\client\\program.exe', [
-		`${n}`,
-		'0',
-		`${n}`,
-		'pi.txt',
-		'table.txt',
-		'0',
-		`${upper}`,
-		'pi.txt'
-	]);
+	await exec('', [`${n}`, '0', `${n}`, 'pi.txt', 'table.txt', '0', `${upper}`, 'pi.txt']);
 
 	rename('./pi.txt', './src/pi.txt', function (err) {
 		if (err) console.log(err);
