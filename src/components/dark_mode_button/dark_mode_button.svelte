21d72377dfba5baa80345e93dc9f1bbb257809ca
Patryk <prusak.patryk@accenture.com>
2023-02-14 21:55:02 +0100

    changes

--- src/components/dark_mode_button/dark_mode_button.svelte
+++ src/components/dark_mode_button/dark_mode_button.svelte
@@ -13,4 +13,4 @@ document.documentElement.classList.remove('dark');return ('light');
     }
 </script>
 
-<button on:click={handleChangeLanguage} class="absolute l-0 top-0 my-8 mx-8 border-black dark:bg-stone-800 dark:hover:bg-slate-300 dark:hover:text-black hover:bg-black hover:text-white rounded-md transition duration-700 dark:text-slate-300 dark:border-slate-300 border-2 font-mono px-2 bg-white text-black py-2">Dark Mode</button>
+<button on:click={handleChangeLanguage} class="absolute l-0 top-0 sm:my-8 my-3  mx-2 sm:mx-8 text-sm sm:text-lg border-black dark:bg-stone-800 dark:hover:bg-slate-300 dark:hover:text-black hover:bg-black hover:text-white rounded-md transition duration-700 dark:text-slate-300 dark:border-slate-300 border-2 font-mono sm:px-2 px-1 py-1 bg-white text-black sm:py-2">Dark Mode</button>
--- src/components/pi/output/output.svelte
+++ src/components/pi/output/output.svelte
@@ -8,14 +8,14 @@
 		<textarea
 			disabled
 			bind:value={pi}
-			class="h-36 w-full self-start rounded-md border-2 border-black hover:shadow-md dark:border-slate-300 dark:text-slate-300 dark:bg-zinc-700"
+			class="h-36 w-full self-start rounded-md border-2 border-black hover:shadow-md dark:border-slate-300 dark:text-slate-300 dark:bg-zinc-700 bg-white"
 		/>
 	</div>
 	<div class="px-2 py-1">
 		<textarea
 			disabled
 			bind:value={table}
-			class="h-36 w-full self-end rounded-md border-2 border-black hover:shadow-md dark:border-slate-300 dark:text-slate-300 dark:bg-zinc-700"
+			class="h-36 w-full self-end rounded-md border-2 border-black hover:shadow-md dark:border-slate-300 dark:text-slate-300 dark:bg-zinc-700 bg-white"
 		/>
 	</div>
 </div>
