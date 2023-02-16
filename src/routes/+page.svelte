caa2c739909663013d95d712f8b5d8ac851344d1
Patryk <prusak.patryk@accenture.com>
2023-02-16 15:32:13 +0100

    :)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 	import { HEADER_TITLE, HEADER_DESCRIPTION } from '../content/header/header_content';
 </script>
 
-<div class="flex max-h-screen w-full flex-col">
+<div class={`flex h-max min-h-screen w-full flex-col bg-cover transition-colors duration-700 ${subsTheme==='dark'?"bg-[url('bg-dark-new.svg')]":"bg-[url('bg-wave-light.svg')]"}`} >
 	<Header title={HEADER_TITLE} description={HEADER_DESCRIPTION} />
 	<PiMainContainer />
 </div>
