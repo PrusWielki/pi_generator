e3823079d47d37e0756d30296468a58777afeb26
Patryk <prusak.patryk@accenture.com>
2023-02-25 17:42:13 +0100

    remove about button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,7 +35,6 @@ document.documentElement.classList.add('dark')
 <div class={`flex h-max min-h-screen w-full flex-col bg-cover transition-colors duration-700 ${subsTheme==='dark'?"bg-[url('/bg-dark-new.svg')]":"bg-[url('/bg-wave-light.svg')]"}`} >
 	<Header title={HEADER_TITLE} description={HEADER_DESCRIPTION} />
 	<DarkModeButton/>
-	<OpenShelfButton bind:openAbout/>
 
 	<ShelfAboutTable bind:openAbout />
 	<PiMainContainer />
