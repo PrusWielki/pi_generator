2adb8048a9cefff7aa861c537289d85aadbaee38
Patryk <pruswielki@gmail.com>
2023-03-07 17:33:34 +0100

    Create container for pi numbers

---
+++ src/components/pi_numbers_effect/pi_numbers_effect.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<div id="pi_numbers_effect_container" class="fixed bottom-0 z-10 h-36 w-full bg-black">
+	<div>1</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	
-
 	// Components
 	import Header from '../components/header/header.svelte';
 	import PiMainContainer from '../components/pi/pi_main_container/pi_main_container.svelte';
@@ -8,6 +6,7 @@
 	import OpenShelfButton from '../components/open_shelf_button/open_shelf_button.svelte';
 	import ShelfAboutTable from '../components/shelf/shelf_about_table.svelte';
 	import Footer from '../components/footer/footer.svelte';
+	import PiNumbersEffect from '../components/pi_numbers_effect/pi_numbers_effect.svelte';
 	// Content
 	import { HEADER_TITLE, HEADER_DESCRIPTION } from '../content/header/header_content';
 	// Assets
@@ -18,24 +17,26 @@
 	let subsTheme: string = 'dark';
 	let openAbout: boolean = false;
 
-	theme.subscribe(value => {
+	theme.subscribe((value) => {
 		subsTheme = value;
 	});
-	if(typeof document!=='undefined'){
-	if(subsTheme==='dark')
-document.documentElement.classList.add('dark')
-            else
-                document.documentElement.classList.remove('dark')}
+	if (typeof document !== 'undefined') {
+		if (subsTheme === 'dark') document.documentElement.classList.add('dark');
+		else document.documentElement.classList.remove('dark');
+	}
 
-	$: console.log(openAbout)
-            
-
+	$: console.log(openAbout);
 </script>
 
-<div class={`flex h-max min-h-screen w-full flex-col bg-cover transition-colors duration-700 ${subsTheme==='dark'?"bg-[url('/bg-dark-new.svg')]":"bg-[url('/bg-wave-light.svg')]"}`} >
+<div
+	class={`flex h-max min-h-screen w-full flex-col bg-cover transition-colors duration-700 ${
+		subsTheme === 'dark' ? "bg-[url('/bg-dark-new.svg')]" : "bg-[url('/bg-wave-light.svg')]"
+	}`}
+>
 	<Header title={HEADER_TITLE} description={HEADER_DESCRIPTION} />
-	<DarkModeButton/>
+	<DarkModeButton />
 
 	<ShelfAboutTable bind:openAbout />
 	<PiMainContainer />
+	<PiNumbersEffect />
 </div>
