fc5ed8eb65ae4f2d18f622b1977f3eee3ab73b6c
Patryk <pruswielki@gmail.com>
2023-03-07 17:20:50 +0100

    :)

--- src/components/pi/pi_main_container/pi_main_container.svelte
+++ src/components/pi/pi_main_container/pi_main_container.svelte
@@ -29,42 +29,58 @@
 			.catch(() => {
 				table = '';
 			});
-	let currentTime:number=0;
-	let interval:NodeJS.Timer;
-	const startInterval = ()=>{currentTime = 0;
-		interval = setInterval(function() {
-    currentTime++;
-	console.log(currentTime)
- }, 1000);}
- const endInterval = ()=>{clearInterval(interval);}
+	let currentTime: number = 0;
+	let interval: NodeJS.Timer;
+	const startInterval = () => {
+		currentTime = 0;
+		interval = setInterval(function () {
+			currentTime++;
+			console.log(currentTime);
+		}, 1000);
+	};
+	const endInterval = () => {
+		clearInterval(interval);
+	};
 
-	$: if(loading) {
+	$: if (loading) {
 		startInterval();
-	}
-	else{
+	} else {
 		endInterval();
 	}
 </script>
 
 <div class="flex flex-col">
-<div
-	class="mt-8 flex w-full  flex-col items-center justify-center overflow-x-hidden md:flex-row md:items-start"
->
 	<div
-		class={` flex sm:w-1/2 justify-end ${ready ? '' : '-translate-x-full'} transition duration-1000`}
+		class="mt-8 flex w-full  flex-col items-center justify-center overflow-x-hidden md:flex-row md:items-start"
 	>
-		<Input bind:loading />
-	</div>
-	{#if loading == true}
-		<div class="sm:w-1/2 animate-pulse flex flex-col gap-12 items-center dark:sm:text-slate-300 justify-center h-72 text-center font-mono text-4xl font-bold"><div></div>Generating pi...<div>{currentTime}</div></div>
-	{:else}
-		<div class={`sm:w-1/2 items-start ${ready ? '' : 'translate-x-full'} transition duration-1000`}>
-			<Output bind:pi bind:table />
+		<div
+			class={` flex justify-end sm:w-1/2 ${
+				ready ? '' : '-translate-x-full'
+			} transition duration-1000`}
+		>
+			<Input bind:loading />
 		</div>
-	{/if}
-
-</div>
-	{#if currentTime>0&&loading==false}
-	<div class="text-center sm:text-2xl font-mono font-semibold text-white dark:text-cyan-500 mt-2 sm:mt-12">Last Execution Time: {currentTime} s</div>
+		{#if loading == true}
+			<div
+				class="flex h-72 animate-pulse flex-col items-center justify-center gap-12 text-center font-mono text-4xl font-bold sm:w-1/2 dark:sm:text-slate-300"
+			>
+				<div />
+				Generating pi...
+				<div>{currentTime}</div>
+			</div>
+		{:else}
+			<div
+				class={`items-start sm:w-1/2 ${ready ? '' : 'translate-x-full'} transition duration-1000`}
+			>
+				<Output bind:pi bind:table />
+			</div>
+		{/if}
+	</div>
+	{#if currentTime > 0 && loading == false}
+		<div
+			class="mt-2 text-center font-mono font-semibold text-white dark:text-cyan-500 sm:mt-12 sm:text-2xl"
+		>
+			Last Execution Time: {currentTime} s
+		</div>
 	{/if}
 </div>
