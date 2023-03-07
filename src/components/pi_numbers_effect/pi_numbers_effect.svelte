0a0b4e121d8eede7f090df35b136228a106e4627
Patryk <pruswielki@gmail.com>
2023-03-07 17:49:07 +0100

    pi numbers random position

--- src/components/pi_numbers_effect/pi_numbers_effect.svelte
+++ src/components/pi_numbers_effect/pi_numbers_effect.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
+	// TODO: Generate a bunch of numbers, map them, add transition fade with big delay or just modify opacity
+
+	let x = Math.floor(Math.random() * 400);
+	let y = Math.floor(Math.random() * 400);
 </script>
 
 <div id="pi_numbers_effect_container" class="fixed bottom-0 z-10 h-36 w-full bg-black">
-	<div>1</div>
+	<div class={`number fixed z-10 text-white b-${x} l-${y}`} style="bottom: {x}px; left: {y}px ">
+		1
+	</div>
 </div>
